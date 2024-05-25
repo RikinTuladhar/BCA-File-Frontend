5ee792d6bb47794388cf0e2207df626fa1d3be80
Rikin Tuladhar <110732947+RikinTuladhar@users.noreply.github.com>
2024-05-25 12:07:09 +0545

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,36 +1,71 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../GlobalContext/UserDetailsProvider";
+import { useNavigate } from "react-router-dom";
 
-const Table = ({data,recentVisited,bookMarkHanlde}) => {
+const Table = ({ data, recentVisited, bookMarkHanlde }) => {
+  const { token } = useContext(UserContext);
+  const navigate = useNavigate();
   // console.log(data)
   return (
-    
     <div className="w-full min-h-[50vh] table-responsive">
-        <table className="table table-striped table-hover">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">View</th>
-              <th scope="col">BookMark</th>
-            </tr>
-          </thead>
-          <tbody>
-           {data?.length > 0 ? data.map((data,i)=>
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">View</th>
+            <th scope="col">BookMark</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.length > 0 ? (
+            data.map((data, i) => (
               <tr key={i}>
-                <td>{i+1}</td>
+                <td>{i + 1}</td>
                 <td>{data?.name}</td>
-                <td><a onClick={e=> recentVisited(data?.id,data?.name,data?.filePath)} href={`${data?.filePath}`} target="_blank" rel="noopener noreferrer"  >View</a></td>
-                <td className="cursor-pointer" onClick={e=> bookMarkHanlde(data?.id)} >Book Mark</td>
+                <td>
+                  <a
+                    onClick={(e) =>
+                      recentVisited(data?.id, data?.name, data?.filePath)
+                    }
+                    href={`${data?.filePath}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View
+                  </a>
+                </td>
+                {token != null ? (
+                  <td
+                    className="cursor-pointer"
+                    onClick={(e) => bookMarkHanlde(data?.id)}
+                  >
+                    Book Mark
+                  </td>
+                ):(
+                  <td
+                  className="cursor-pointer"
+                  onClick={(e) => navigate("/signin")}
+                >
+                  Book Mark
+                </td>
+                )}
+
+
               </tr>
-           ) : (
+            ))
+          ) : (
             <tr>
-              <td colSpan="4" className="text-lg text-center text-red-700 md:text-2xl">
+              <td
+                colSpan="4"
+                className="text-lg text-center text-red-700 md:text-2xl"
+              >
                 No Data Found
               </td>
             </tr>
-           ) }
-          </tbody>
-        </table>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
